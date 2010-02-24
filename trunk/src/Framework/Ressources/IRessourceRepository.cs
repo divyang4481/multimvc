2900ba11f06f201801197c7c58e1284522162342
geoffrey.vandiest <geoffrey.vandiest@74476332-1fea-11df-b401-3b86f75e9ead>
2010-02-24 17:02:30 +0000

    #1

---
+++ trunk/src/Framework/Ressources/IRessourceRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using BA.MultiMVC.Framework.Core;
+
+namespace BA.MultiMVC.Framework.Ressources
+{
+    public interface IRessourceRepository:IRepository
+    {
+        IDictionary<string, string> Find(string language);
+    }
+}
