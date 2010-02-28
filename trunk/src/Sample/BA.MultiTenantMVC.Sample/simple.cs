2759b9af8fdd3f473f6d52a8025807544d8e8041
geoffrey.vandiest <geoffrey.vandiest@74476332-1fea-11df-b401-3b86f75e9ead>
2010-02-28 14:16:17 +0000

    Modify (Add)Controller template

---
+++ trunk/src/Sample/BA.MultiTenantMVC.Sample/simple.cs
@@ -0,0 +1,9 @@
+﻿
+public class Test
+{
+		 public int Prop0 {get;set;}
+		 public int Prop1 {get;set;}
+		 public int Prop2 {get;set;}
+		 public int Prop3 {get;set;}
+		 public int Prop4 {get;set;}
+	}
