4f2ca5e829b03f4f868b97ddf31ff62e485d1613
geoffrey.vandiest <geoffrey.vandiest@74476332-1fea-11df-b401-3b86f75e9ead>
2010-10-24 16:43:23 +0000

    Create Solution BackToOwner V3.0.1

--- trunk/src/Framework/ICacheService.cs
+++ trunk/src/Framework/ICacheService.cs
@@ -5,7 +5,6 @@ namespace BA.MultiMvc.Framework
     public interface ICacheService:ITenantModel
     {
         object GetObject(string key);
-
         void Add(string key, object value);
     }
 }
